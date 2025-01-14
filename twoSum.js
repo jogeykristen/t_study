7b56b732c4bfdbf1e2625cd894aa22d311b6f66b
jogey.kristen <jogey.kristen@centelon.com>
2025-01-14 18:40:04 +0530

    added palindrome

--- twoSum.js
+++ twoSum.js
@@ -1,14 +1,29 @@
-var twoSum = (arr1, target) => {
-  console.log("arr1 = ", arr1, "target = ", target);
-  for (let x = 0; x <= arr1.length - 2; x++) {
-    for (let j = x + 1; j <= arr1.length - 1; j++) {
-      if (arr1[x] + arr1[j] == target) {
-        return console.log(`the target numbers are = ${arr1[x]},${arr1[j]}`);
-      }
-    }
-  }
-  console.log("no numbers found");
-};
+// var twoSum = (arr1, target) => {
+//   console.log("arr1 = ", arr1, "target = ", target);
+//   for (let x = 0; x <= arr1.length - 2; x++) {
+//     for (let j = x + 1; j <= arr1.length - 1; j++) {
+//       if (arr1[x] + arr1[j] == target) {
+//         return console.log(`the target numbers are = ${arr1[x]},${arr1[j]}`);
+//       }
+//     }
+//   }
+//   console.log("no numbers found");
+// };
 
-var arr1 = [2, 7, 11, 15];
+// var arr1 = [2, 7, 11, 15];
+// twoSum(arr1, 22);
+
+var twoSum = (arr, target)=>{
+    var result =[]
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]== target){
+                result.push([arr[i],arr[j]])
+            }
+        }
+    }
+    console.log("result ==== ",result)
+}
+
+var arr1 = [2, 7, 11, 15,20];
 twoSum(arr1, 22);
